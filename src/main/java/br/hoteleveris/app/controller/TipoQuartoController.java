1252c38fbfe7f7c15d09ea2ebf15ed66a2134ed9
Eduardo Andrade Santos <eandrasa@everis.com>
2020-11-12 16:01:56 -0300

    TipoQuartoController e TipoQuartoService add

---
+++ src/main/java/br/hoteleveris/app/controller/TipoQuartoController.java
@@ -0,0 +1,50 @@
+package br.hoteleveris.app.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import br.hoteleveris.app.request.TipoQuartoRequest;
+import br.hoteleveris.app.response.BaseResponse;
+import br.hoteleveris.app.service.TipoQuartoService;
+
+@RestController
+@RequestMapping("/tipoquarto")
+public class TipoQuartoController extends BaseController {
+
+	@Autowired
+	TipoQuartoService _service;
+
+	// POST - CRIAR UM CLIENTE
+	@PostMapping
+	public ResponseEntity<BaseResponse> criar(@RequestBody TipoQuartoRequest request) {
+		try {
+			BaseResponse response = _service.criar(request);
+			return ResponseEntity.status(response.statusCode).body(response);
+		} catch (Exception e) {
+			return ResponseEntity.status(errorBase.statusCode).body(errorBase);
+		}
+	}
+
+	// GET - OBTER POR ID
+	@GetMapping(path = "/{id}")
+	public ResponseEntity<BaseResponse> obter(@PathVariable Long id) {
+		try {
+			BaseResponse response = _service.obter(id);
+			return ResponseEntity.status(response.statusCode).body(response);
+		} catch (Exception e) {
+			return ResponseEntity.status(errorBase.statusCode).body(errorBase);
+		}
+	}
+
+	// GET - OBTER TUDO
+	@GetMapping
+	public ResponseEntity<BaseResponse> listar() {
+		try {
+			BaseResponse response = _service.listar();
+			return ResponseEntity.status(response.statusCode).body(response);
+		} catch (Exception e) {
+			return ResponseEntity.status(errorBase.statusCode).body(errorBase);
+		}
+	}
+}
