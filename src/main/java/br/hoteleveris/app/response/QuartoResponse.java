7143afdbb66089404a459e4e3b6a19c737c141c9
Eduardo Andrade Santos <eandrasa@everis.com>
2020-11-12 15:26:09 -0300

    TipoQuartoResponse add

---
+++ src/main/java/br/hoteleveris/app/response/QuartoResponse.java
@@ -0,0 +1,5 @@
+package br.hoteleveris.app.response;
+
+public class QuartoResponse extends BaseResponse {
+
+}
---
+++ src/main/java/br/hoteleveris/app/response/TipoQuartoResponse.java
@@ -0,0 +1,32 @@
+package br.hoteleveris.app.response;
+
+public class TipoQuartoResponse extends BaseResponse {
+	public Long id;
+	public String descricao;
+	public Double valor;
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getDescricao() {
+		return descricao;
+	}
+
+	public void setDescricao(String descricao) {
+		this.descricao = descricao;
+	}
+
+	public Double getValor() {
+		return valor;
+	}
+
+	public void setValor(Double valor) {
+		this.valor = valor;
+	}
+
+}
